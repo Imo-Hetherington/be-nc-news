9f30a7b6604f34c3e27f5e90eed3f7b7643d8250
Imo-Hetherington <hetherington.ir@gmail.com>
2020-02-10 15:20:48 +0000

    Created formatDates file which returns javascript data from unix timestamp.

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,12 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const newList = list.map(object => {
+    let objectCopy = { ...object };
+    objectCopy.created_at = new Date(object.created_at);
+    return objectCopy;
+  });
+
+  return newList;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,78 @@
-const { expect } = require('chai');
-const {
-  formatDates,
-  makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+const { expect } = require("chai");
+const { formatDates } = require("../db/utils/utils");
 
-describe('formatDates', () => {});
-
-describe('makeRefObj', () => {});
-
-describe('formatComments', () => {});
+describe("formatDates", () => {
+  it("Given a single object in an array, will return an array", () => {
+    const testArr = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(formatDates(testArr)).to.be.an("array");
+    expect(formatDates(testArr)[0]).to.be.an("object");
+  });
+  it("Will convert timestamp in a given object to a date", () => {
+    const testArr = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(formatDates(testArr)[0].created_at).to.eql(new Date(1542284514171));
+  });
+  it("Given an array of multiple objects, will convert all timestamps to dates", () => {
+    const testArr = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ];
+    expect(formatDates(testArr)[1].created_at).to.eql(new Date(1511354163389));
+  });
+  it("Will not mutate or reference original array.", () => {
+    const testArr = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(formatDates(testArr)).not.to.equal(testArr);
+    expect(formatDates(testArr)[0]).not.to.equal(testArr[0]);
+    formatDates(testArr);
+    expect(testArr).to.eql([
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ]);
+  });
+});
