a0f6ce4fb7c80c978832911ce379862b065e2efa
Imo-Hetherington <hetherington.ir@gmail.com>
2020-02-11 15:43:49 +0000

    Amended data file to access test data in the spec file.

--- db/data/index.js
+++ db/data/index.js
@@ -4,18 +4,8 @@ const testData = require("./test-data/index");
 const ENV = process.env.NODE_ENV || "development";
 
 const data = {
-  development: {
-    articleData: devData.articleData,
-    commentData: devData.commentData,
-    topicData: devData.topicData,
-    userData: devData.userData
-  },
-  test: {
-    articleData: devData.articleData,
-    commentData: devData.commentData,
-    topicData: devData.topicData,
-    userData: devData.userData
-  }
+  development: devData,
+  test: testData
 };
 
 module.exports = data[ENV];
