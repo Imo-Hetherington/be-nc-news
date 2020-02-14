0cd909ab0e182552b9dbfca71243c47044fdbf81
Imo-Hetherington <hetherington.ir@gmail.com>
2020-02-14 08:59:11 +0000

    Removed duplicate controller functions.

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -23,23 +23,6 @@ exports.getCommentsByArticle = (req, res, next) => {
     })
     .catch(err => next(err));
 };
-exports.postCommentToArticle = (req, res, next) => {
-  const { article_id } = req.params;
-  addComment(req.body, article_id)
-    .then(([comment]) => {
-      res.status(201).send({ comment });
-    })
-    .catch(err => next(err));
-};
-
-exports.getCommentsByArticle = (req, res, next) => {
-  const { article_id } = req.params;
-  Promise.all([fetchComments(article_id, req.query), fetchArticle(article_id)])
-    .then(([comments]) => {
-      res.status(200).send({ comments });
-    })
-    .catch(err => next(err));
-};
 
 exports.patchCommentVotes = (req, res, next) => {
   const { inc_votes } = req.body;
